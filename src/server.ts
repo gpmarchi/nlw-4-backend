240ffd2f4bd59f0ea38e27e9b514badbd2495861
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-02-23 13:01:02 -0300

    User creation feature

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,13 @@
+import "reflect-metadata";
 import express from 'express';
+import "./database";
+import { router } from "./routes";
 
 const PORT = 3333;
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "example GET method" });
-});
-
-app.post("/", (request, response) => {
-  return response.json({ message: "example POST method" });
-})
+app.use(express.json());
+app.use(router);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
