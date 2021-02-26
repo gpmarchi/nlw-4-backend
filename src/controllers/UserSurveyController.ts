40fb6ad5fc88c3a6c11c1f3f2d186cf3a3f6ef78
gpmarchi <42723081+gpmarchi@users.noreply.github.com>
2021-02-26 10:35:07 -0300

    Refactor to allow for a new user/survey

--- src/controllers/UserSurveyController.ts
+++ src/controllers/UserSurveyController.ts
@@ -26,25 +26,23 @@ class UserSurveyController {
       response.status(400).json({ error: "Survey does not exists." });
     }
 
+    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
+
+    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
+      where: { user_id: user.id, value: null },
+      relations: ["user", "survey"],
+    });
+
     const variables = {
       name: user.name,
       title: survey.title,
       description: survey.description,
-      user_id: user.id,
+      id: "",
       link: process.env.URL_MAIL,
     };
 
-    const npsPath = resolve(__dirname, "..", "views", "emails", "npsMail.hbs");
-
-    const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-      where: [
-        { user_id: user.id },
-        { value: null }
-      ],
-      relations: ["user", "survey"],
-    });
-
     if (surveyUserAlreadyExists) {
+      variables.id = surveyUserAlreadyExists.id;
       await SendMailProvider.execute(email, survey.title, variables, npsPath);
       return response.json(surveyUserAlreadyExists);
     }
@@ -55,6 +53,8 @@ class UserSurveyController {
     });
 
     await surveysUsersRepository.save(surveyUser);
+
+    variables.id = surveyUser.id;
 
     await SendMailProvider.execute(email, survey.title, variables, npsPath);
 
